b72880bc4aaa6ce981ca5167ce8deb320a6db476
howflyquad <howflyquad@gmail.com>
2017-06-19 22:15:23 -0400

    Sync

--- src/delta5server/Delta5Race.py
+++ src/delta5server/Delta5Race.py
@@ -3,7 +3,9 @@
 class Delta5Race():
     '''Class to hold race management variables.'''
     def __init__(self):
+        self.num_nodes = 0
         self.current_heat = 1
+        self.race_status = False
 
 def get_race_state():
     '''Returns the delta 5 race object.'''
--- src/delta5server/server.py
+++ src/delta5server/server.py
@@ -1,9 +1,8 @@
-'''Delta5 timing system server script'''
+'''Delta5 race timer server script'''
 
 import os
 import sys
 from datetime import datetime
-from datetime import timedelta
 
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
@@ -30,10 +29,10 @@ APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 DB = SQLAlchemy(APP)
 
 INTERFACE = get_hardware_interface()
-RACE = get_race_state()
+RACE = get_race_state() # For storing race management variables
 
 PROGRAM_START = datetime.now()
-RACE_START = datetime.now()
+RACE_START = datetime.now() # Updated on race start commands
 
 #
 # Database Models
@@ -110,11 +109,6 @@ def settings():
 
 # Debug Routes
 
-@APP.route('/timingserver')
-def timingserver():
-    '''Route to timing server page.'''
-    return render_template('timingserver.html', async_mode=SOCKET_IO.async_mode)
-
 @APP.route('/database')
 def database():
     '''Route to database page.'''
@@ -134,11 +128,8 @@ def connect_handler():
     global HEARTBEAT_THREAD
     if HEARTBEAT_THREAD is None:
         HEARTBEAT_THREAD = gevent.spawn(heartbeat_thread_function)
-    emit_channel()
-    emit_trigger_rssi()
-    emit_peak_rssi()
-
-    emit_current_laps()
+    emit_node_data()
+    # emit_current_laps()
 
 @SOCKET_IO.on('disconnect')
 def disconnect_handler():
@@ -149,19 +140,16 @@ def disconnect_handler():
 
 @SOCKET_IO.on('set_frequency')
 def on_set_frequency(data):
-    '''Gets a node index number and frequency to update on the node.'''
+    '''Gets node index and frequency to update a node.'''
     node_index = data['node']
     frequency = data['frequency']
     server_log('Set Frequency: Node {0} Frequency {1}'.format(node_index, frequency))
-    # emit('frequency_set', {'node': node_index, 'frequency': \
-    #     INTERFACE.set_full_reset_frequency(node_index, frequency)}, broadcast=True)
     INTERFACE.set_frequency(node_index, frequency)
-    emit('frequency_set', INTERFACE.get_frequency_json(index), broadcast=True)
-    emit_channel()
+    emit_node_data()
 
 @SOCKET_IO.on('set_pilot_position')
 def on_set_pilot_position(data):
-    '''Gets a node index number and frequency to update on the node.'''
+    '''Gets heat index, node index, and pilot it to update database.'''
     heat = data['heat']
     node = data['node']
     pilot = data['pilot']
@@ -174,82 +162,65 @@ def on_set_pilot_position(data):
 
 @SOCKET_IO.on('start_race')
 def on_start_race():
-    '''Starts the race.'''
-    print 'Start Race'
-    INTERFACE.set_race_status(1) # Start registering laps
+    '''Starts the race and the timer counting up, no defined finish.'''
+    INTERFACE.enable_calibration_mode() # Prep nodes to reset triggers on next pass
     gevent.sleep(0.500) # Make this random 2 to 5 seconds
-    SOCKET_IO.emit('start_timer')
-    RACE_START = datetime.now()
+    SOCKET_IO.emit('start_timer') # Loop back to race page to start the timer counting up
+    RACE.race_status = True # To enable registering passed laps
+    RACE_START = datetime.now() # Update the race start time stamp
     server_log('Race started at {0}'.format(RACE_START))
-    emit_trigger_rssi() # Just to see the values on the start line
-    emit_peak_rssi()
+    emit_node_data() # Just to see the values on the start line
 
 @SOCKET_IO.on('start_race_2_min')
 def on_start_race_2_min():
     '''Starts the race with a two minute countdown clock.'''
-    # On starting a race, have a 2 to 5 second delay here after setting the hardware interface
-    # setting to true and then sound the start buzzer tied into the clock function
-    # stopping the race should stop and reset the timer
-    print 'Start Race'
-    INTERFACE.set_race_status(1)
+    INTERFACE.enable_calibration_mode() # Prep nodes to reset triggers on next pass
     gevent.sleep(0.500) # Make this random 2 to 5 seconds
-    SOCKET_IO.emit('start_timer_2min')
-    RACE_START = datetime.now()
+    SOCKET_IO.emit('start_timer_2min') # Loop back to race page to start a 2 min countdown
+    RACE.race_status = True # To enable registering passed laps
+    RACE_START = datetime.now() # Update the race start time stamp
     server_log('Race started at {0}'.format(RACE_START))
-    emit_trigger_rssi() # Just to see the values on the start line
-    emit_peak_rssi()
+    emit_node_data() # Just to see the values on the start line
 
 @SOCKET_IO.on('stop_race')
 def on_race_status():
-    '''Stops the racing and sets the hardware to stop looking for laps.'''
+    '''Stops the racing and stops looking for laps.'''
     server_log('Race stopped')
-    INTERFACE.set_race_status(0)
+    RACE.race_status = False # To stop registering passed laps
 
 @SOCKET_IO.on('save_laps')
 def on_save_laps():
-    '''Command to save current laps data to the database and clear the current laps.'''
+    '''Command to save current laps to the database and clear the current laps.'''
 
 @SOCKET_IO.on('clear_laps')
 def on_clear_laps():
     '''Command to clear the current laps due to false start or practice.'''
 
-# Timingserver events
+# Socket io emit functions
 
-@SOCKET_IO.on('get_settings')
-def on_get_settings():
-    return INTERFACE.get_settings_json()
+def emit_node_data():
+    '''Emits node data.'''
+    SOCKET_IO.emit('node_data', {
+        'frequency': [node.frequency for node in INTERFACE.nodes],
+        'channel': [Frequency.query.filter_by(frequency=node.frequency).first().channel \
+            for node in INTERFACE.nodes],
+        'trigger_rssi': [node.trigger_rssi for node in INTERFACE.nodes],
+        'peak_rssi': [node.peak_rssi for node in INTERFACE.nodes]
+    })
 
-@SOCKET_IO.on('set_calibration_threshold')
-def on_set_calibration_threshold(data):
-    server_log(data)
-    calibration_threshold = data['calibration_threshold']
-    INTERFACE.set_calibration_threshold_global(calibration_threshold)
-    emit('calibration_threshold_set', INTERFACE.get_calibration_threshold_json(), broadcast=True)
-
-@SOCKET_IO.on('set_calibration_offset')
-def on_set_calibration_offset(data):
-    server_log(data)
-    calibration_offset = data['calibration_offset']
-    INTERFACE.set_calibration_offset_global(calibration_offset)
-    emit('calibration_offset_set', INTERFACE.get_calibration_offset_json(), broadcast=True)
-
-@SOCKET_IO.on('set_trigger_threshold')
-def on_set_trigger_threshold(data):
-    server_log(data)
-    trigger_threshold = data['trigger_threshold']
-    INTERFACE.set_trigger_threshold_global(trigger_threshold)
-    emit('trigger_threshold_set', INTERFACE.get_trigger_threshold_json(), broadcast=True)
-
-@SOCKET_IO.on('enable_calibration_mode')
-def on_enable_calibration_mode():
-    INTERFACE.enable_calibration_mode();
-
-@SOCKET_IO.on('simulate_pass')
-def on_simulate_pass(data):
-    server_log(data)
-    index = data['node']
-    # todo: how should frequency be sent?
-    emit('pass_record', {'node': index, 'frequency': INTERFACE.nodes[index].frequency, 'timestamp': milliseconds()}, broadcast=True)
+def emit_current_laps():
+    '''Emits current_laps json.'''
+    current_laps = []
+    # for node in DB.session.query(CurrentLap.node_index).distinct():
+    for node in range(RACE.num_nodes):
+        node_laps = []
+        node_lap_times = []
+        for lap in CurrentLap.query.filter_by(node_index=node).all():
+            node_laps.append(lap.lap_id)
+            node_lap_times.append(time_format(lap.lap_time))
+        current_laps.append({'lap_id': node_laps, 'lap_time': node_lap_times})
+    current_laps = {'node_index': current_laps}
+    SOCKET_IO.emit('current_laps', current_laps)
 
 #
 # Program Functions
@@ -268,11 +239,12 @@ def ms_from_race_start():
         * 1000 + delta_time.microseconds / 1000.0
     return milli_sec
 
-def milliseconds():
+def ms_from_program_start():
     '''Returns the elapsed milliseconds since the start of the program.'''
-    dt = datetime.now() - PROGRAM_START
-    ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
-    return ms
+    delta_time = datetime.now() - PROGRAM_START
+    milli_sec = (delta_time.days * 24 * 60 * 60 + delta_time.seconds) \
+        * 1000 + delta_time.microseconds / 1000.0
+    return milli_sec
 
 def time_format(millis):
     '''Convert milliseconds to 00:00.000'''
@@ -287,11 +259,12 @@ def time_format(millis):
 def pass_record_callback(node, ms_since_lap):
     '''Logs and emits a completed lap.'''
 
-    print('Pass record from {0}{1}: {2}, {3}'.format(node.index, node.frequency, ms_since_lap, milliseconds() - ms_since_lap))
+    print 'Pass record from {0} {1}: {2}, {3}'.format(node.index, node.frequency, ms_since_lap, \
+        ms_from_program_start() - ms_since_lap)
     SOCKET_IO.emit('pass_record', {
         'node': node.index,
         'frequency': node.frequency,
-        'timestamp': milliseconds() - ms_since_lap,
+        'timestamp': ms_from_program_start() - ms_since_lap,
         'trigger_rssi': node.trigger_rssi,
         'peak_rssi_raw': node.peak_rssi_raw,
         'peak_rssi': node.peak_rssi})
@@ -339,55 +312,36 @@ INTERFACE.pass_record_callback = pass_record_callback
 def server_log(message):
     '''Messages emitted from the server script.'''
     print message
-    SOCKET_IO.emit('server_log', message)
+    SOCKET_IO.emit('hardware_log', message)
 
 def hardware_log_callback(message):
     '''Message emitted from the delta 5 interface class.'''
-    print(message)
+    print message
     SOCKET_IO.emit('hardware_log', message)
 
 INTERFACE.hardware_log_callback = hardware_log_callback
 
-def emit_channel():
-    '''Emits channel json.'''
-    SOCKET_IO.emit('channel', \
-        {'channel': [Frequency.query.filter_by(frequency=node.frequency).first().channel \
-        for node in INTERFACE.nodes], \
-        'frequency': [node.frequency for node in INTERFACE.nodes]})
-
-def emit_trigger_rssi():
-    '''Emits trigger_rssi json.'''
-    SOCKET_IO.emit('trigger_rssi', \
-        {'trigger_rssi': [node.trigger_rssi for node in INTERFACE.nodes]})
-
-def emit_peak_rssi():
-    '''Emits peak_rssi json.'''
-    SOCKET_IO.emit('peak_rssi', \
-        {'peak_rssi': [node.peak_rssi for node in INTERFACE.nodes]})
-
-def emit_current_laps():
-    '''Emits current_laps json.'''
-    current_laps = []
-    # for node in DB.session.query(CurrentLap.node_index).distinct():
-    for node in range(RACE.num_nodes):
-        node_laps = []
-        node_lap_times = []
-        for lap in CurrentLap.query.filter_by(node_index=node).all():
-            node_laps.append(lap.lap_id)
-            node_lap_times.append(time_format(lap.lap_time))
-        current_laps.append({'lap_id': node_laps, 'lap_time': node_lap_times})
-    current_laps = {'node_index': current_laps}
-    SOCKET_IO.emit('current_laps', current_laps)
+def default_frequencies():
+    '''Set each nodes frequency, use imd frequncies for 6 or less and race band for 7 or 8'''
+    server_log('Setting default frequencies')
+    frequencies_imd_5_6 = [5685, 5760, 5800, 5860, 5905, 5645]
+    frequencies_raceband = [5658, 5695, 5732, 5769, 5806, 5843, 5880, 5917]
+    for index, node in enumerate(INTERFACE.nodes):
+        gevent.sleep(0.100)
+        if RACE.num_nodes < 7:
+            INTERFACE.set_frequency(index, frequencies_imd_5_6[index])
+        else:
+            INTERFACE.set_frequency(index, frequencies_raceband[index])
 
 #
 # Program Initialize
 #
 
-
-# gevent.sleep(0.500) # Delay to get I2C addresses
-# INTERFACE.default_frequencies()
-
 RACE.num_nodes = len(INTERFACE.nodes)
+print 'Number of nodes found: {0}'.format(RACE.num_nodes)
+
+gevent.sleep(0.500) # Delay to get I2C addresses
+default_frequencies()
 
 DB.session.query(CurrentLap).delete() # Clear out the current laps table
 DB.session.commit()
